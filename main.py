508334018577282f80f3ccbb11b2687978e9bdab
Michael Taylor <met3530@rit.edu>
2021-07-14 04:45:26 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+def main():
+    print("Hello World!")
+    res = int(input("Enter a num: "))
+    print("You entered: " + str(res))
+    print("That plus 5 is: " + str(res+5))
+
+
+if __name__ == "__main__":
+    main()
